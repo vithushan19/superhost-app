13f06bf87d2f0c7e037f87a0d9faf3f55b594d5c
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2022-08-13 14:46:54 -0400

    update

--- components/CardDetails.js
+++ components/CardDetails.js
@@ -1,8 +1,8 @@
 const CardDetails = ({ text, icon }) => (
-    <span className="bg-gradient-to-r from-gray-100 to-gray-300 text-stone-900 text-xs font-medium inline-flex items-center px-2.5 py-1 rounded mr-2">
+    <div className="bg-gradient-to-r from-gray-100 to-gray-300 text-stone-900 text-xs font-medium flex items-center justify-center px-2.5 py-1 rounded">
       {icon}
       {text}
-    </span>
+    </div>
 )
 
 export default CardDetails
--- components/LocationCardDetails.js
+++ components/LocationCardDetails.js
@@ -1,9 +1,9 @@
 const LocationCardDetails = ({ text, icon }) => (
     <a href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(text)}`}>
-    <span className="bg-gradient-to-r from-gray-100 to-gray-300 text-stone-900 text-xs font-medium inline-flex items-center px-2.5 py-1 rounded mr-2">
+    <div className="bg-gradient-to-r from-gray-100 to-gray-300 text-stone-900 text-xs font-medium flex items-center justify-center px-2.5 py-1 rounded">
         {icon}
         <u>{text}</u>
-    </span>
+    </div>
     </a>
 )
 
