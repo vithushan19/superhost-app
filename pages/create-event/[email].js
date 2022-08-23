b43940972c2ed587f4eb805cf52ea74d84d3ec97
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2022-08-23 09:09:21 -0400

    change text color for form

--- pages/create-event/[email].js
+++ pages/create-event/[email].js
@@ -126,17 +126,14 @@ const CreateEvent = () => {
           <PlacesAutocomplete setLocation={setEventLocation} location={eventLocation} />
         </div>
         <div className='flex flex-row gap-4 items-center self-end'>
-          <p className="text-black font-medium">From</p> <DateTimePicker date={startDate} setDate={setStartDate} />
+          <p className="text-gray-300 font-medium">From</p> <DateTimePicker date={startDate} setDate={setStartDate} />
         </div>
         <div className='flex flex-row gap-4 items-center self-end'>
-          <p className="text-black font-medium">To</p><DateTimePicker date={endDate} setDate={setEndDate} />
+          <p className="text-gray-300 font-medium">To</p><DateTimePicker date={endDate} setDate={setEndDate} />
         </div>
         <div id="fileUpload" className='text-left'>
-          <div className='mb-2 block'>
-            <Label htmlFor="file" value="Upload your image" />
-          </div>
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="file_input">Upload file</label>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="file_input">Upload Invitation Image</label>
             <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" onChange={(event) => { setImage(event.target.files[0]) }} />
             <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
           </div>
