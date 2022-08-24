af0a58b98ac22d4b872eed5e15eaa6876b402d30
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2022-08-24 08:24:41 -0400

    update logo

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,6 @@ function MyApp({ Component, pageProps }) {
       <Flowbite>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-          <meta name="description" content="Effortless Event Planning." />
-          <title>Superhost</title>
-          <link rel="icon" type="image/png" sizes="32x32" href="/images/superhost-icon.png" />
         </Head>
         <Component {...pageProps} />
       </Flowbite>
--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,11 @@ export default function Home() {
 
   return (
     <div className="py-0 px-8 bg-gray-800">
+      <Head>
+        <meta name="description" content="Effortless Event Planning." />
+        <title>Superhost</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/superhost-icon.png" />
+      </Head>
       <main className="flex flex-col items-center justify-center h-screen gap-5">
         <h6 className='text-white text-2xl uppercase font-semibold'>Superhost App</h6>
         <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={() => { setShowLoginModal(true) }}>
