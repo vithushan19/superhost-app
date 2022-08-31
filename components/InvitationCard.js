a5a64bf4528ef19845e0d52802904fc2d52e29a9
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2022-08-31 09:52:26 -0400

    update logic

--- components/InvitationCard.js
+++ components/InvitationCard.js
@@ -14,9 +14,9 @@ const FullScreenOverlayCard = ({ title, imageURL, message, location, dateDetails
                 </p>
             </div>
             <div className="flex flex-col items-stretch pb-5 px-2">
-                <div className="w-full border-stone-900 bg-stone-100 border-2 mb-5 pl-5 py-1 rounded shadow-lg inline-flex items-center">
+                { message.length > 0 && <div className="w-full border-stone-900 bg-stone-100 border-2 mb-5 pl-5 py-1 rounded shadow-lg inline-flex items-center">
                     <p className="text-stone-900 font-bold text-left text-lg font-montserrat">{message}</p>
-                </div>
+                </div>}
                 <CardDetails
                     text={dateDetails}
                     icon={<ClockIcon className="mr-1 h-5 w-5 text-slate-900" />}
@@ -49,9 +49,9 @@ const CenteredOverlayCard = ({ title, imageURL, message, location, dateDetails, 
                 <img src={imageURL} alt="invitation image" className="px-2 w-full rounded shadow-xl" style={{ maxHeight: '60vh' }} />
             </picture>
             <div className="flex flex-col shadow items-stretch px-2">
-                <div className="mt-5 mb-5 border-2 bg-stone-100 border-stone-900 w-full py-1 rounded shadow-lg">
+                { message.length > 0 && <div className="mt-5 mb-5 border-2 bg-stone-100 border-stone-900 w-full py-1 rounded shadow-lg">
                     <p className="text-slate-900 text-lg font-semibold text-left font-montserrat px-2">{message}</p>
-                </div>
+                </div>}
                 <CardDetails
                     text={dateDetails}
                     icon={<ClockIcon className="mr-1 h-5 w-5 text-slate-900" />}
