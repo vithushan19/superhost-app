a9157c6861fe22200d9ab4679d14fc1113f63480
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2022-09-08 00:07:57 -0400

    update margin bottom on form

--- pages/create-event.js
+++ pages/create-event.js
@@ -116,7 +116,7 @@ const CreateEvent = () => {
             </button> :
             <button type='submit' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 absolute right-0" form='neweventform'>Finalize Design</button>
         }
-        <form id="neweventform" className='flex flex-col items-stretch gap-4 mt-12 mx-3' onSubmit={onFormSubmit}>
+        <form id="neweventform" className='flex flex-col items-stretch gap-4 mt-12 mx-3 mb-2' onSubmit={onFormSubmit}>
           <div className='text-left'>
             <div className="mb-2 block">
               <Label
