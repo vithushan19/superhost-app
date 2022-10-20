b7fc7edc9a262c210965b2c542effa733dfdb981
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-10-20 00:07:08 -0400

    update invitation

--- components/InvitationCard.js
+++ components/InvitationCard.js
@@ -17,34 +17,29 @@ const FullScreenOverlayCard = ({ title, imageURL, location, dateDetails, primary
                 </p>
             </div>
             <div className="flex flex-col items-stretch px-2 pb-5">
-                <div className="p-4 mb-2 font-medium rounded bg-gradient-to-r from-gray-200 to-gray-400 text-slate-900">
-                    <div className='flex items-center'>
-                    <ClockIcon className="w-5 h-5 mr-1 text-slate-900" />
-                    Schedule
+                <div className="p-3 mb-2 font-medium text-white border-2 border-white rounded bg-opacity-60 bg-slate-900">
+                    <div className='flex justify-center'>
+                    <div className='flex items-center font-bold'>
+                        <ClockIcon className="w-5 h-5 mr-1 text-white" />
+                        Schedule
                     </div>
+                    </div>
+                    <p>PRE WEDDING</p>
                     <p>5:30 Paal Ceremony at Grooms House</p>
                     <p>7:30 Bride Side Photos at Brides House</p>
                     <p>8:00 Groom Side Photos at Grooms House</p>
                     <p>8:30 Bride Side Goes to the Hall</p>
                     <p>9:00 Grooms Side Goes to the Hall</p>
+                    <p className='mt-2'>WEDDING</p>
                     <p>9:00 Brides First Entrance</p>
                     <p>9:30 Grooms Entrance</p>
+                    <p>11:00 Brides Second Entrance</p>
                     <p>11:30 Thali Time</p>
                     <p>12:00 Registration</p>
                     <p>12:15 Lunch and Photos</p>
                     <p>14:30 End </p>
                 </div>
-                {/* <CardDetails
-                    text={dateDetails}
-                    icon={<ClockIcon className="w-5 h-5 mr-1 text-slate-900" />}
-                /> */}
-                <LocationCardDetails
-                    text={location}
-                    icon={
-                        <LocationMarkerIcon className="w-5 h-5 mr-1 text-slate-900" />
-                    }
-                />
-                <div className="my-5">{primaryButton}</div>
+                <div className="mt-4">{primaryButton}</div>
             </div>
         </div>
     </>
